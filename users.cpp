3284281323b4e3a75708830a19aa28eaeaacd2ef
Abdullah Qasim <abdullahqasim00987@gmail.com>
2024-07-01 14:57:02 +0500

    added constructor to payment class in users.cpp

--- users.cpp
+++ users.cpp
@@ -6,6 +6,28 @@ class Payment{
     protected:
         string cardNumber, cardName, cardType;
         int cardYear, cardMonth, cardCvv;
+        float amount;
+
+    public:
+        Payment(){
+            cardNumber = "";
+            cardName = "";
+            cardType = "";
+            cardYear = 0;
+            cardMonth = 0;
+            cardCvv = 0;
+        }
+
+        Payment(string cardNumber, string cardName, string cardType, int cardYear, int cardMonth, int CardCvv){
+            this->cardNumber = cardNumber;
+            this->cardName = cardName;
+            this->cardType = cardType;
+            this->cardYear = cardYear;
+            this->cardMonth = cardMonth;
+            this->cardCvv = cardCvv;
+        }
+
+        
 };
 
 class User{
