f98915fd7796464356a77195ef2997bd3413b3c5
Abdullah Qasim <abdullahqasim00987@gmail.com>
2024-07-01 22:01:15 +0500

    added functions to get month and time in utils.cpp

---
+++ utils.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int getMonth(){
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    return 1 + ltm->tm_mon;
+}
+
+int getDay(){
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    return ltm->tm_mday;
+}
