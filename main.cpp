08853e061a866d74466414e1433e6df62dd92c85
Abdullah Qasim <abdullahqasim00987@gmail.com>
2024-07-01 22:16:10 +0500

    added member function makePayment to payment class

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-#include <users.cpp>
-#include <vehicles.cpp>
+#include "users.cpp"
+#include "vehicles.cpp"
 using namespace std;
 
 int main() {
-    
+    Payment p("1234567890123456", "John Doe", "Visa", 2025, 12, 123);
+    int status = p.makePayment(1000);
+    if(status == 0){
+        cout << "Payment Successful" << endl;
+    }
+    else if(status == -1){
+        cout << "Card Expired" << endl;
+    }
+    else if(status == -2){
+        cout << "Invalid CVV" << endl;
+    }
 }
--- users.cpp
+++ users.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <fstream>
+#include "utils.cpp"
 using namespace std;
 
 class Payment{
     protected:
         string cardNumber, cardName, cardType;
         int cardYear, cardMonth, cardCvv;
-        float amount;
 
     public:
         Payment(){
@@ -18,7 +18,7 @@ class Payment{
             cardCvv = 0;
         }
 
-        Payment(string cardNumber, string cardName, string cardType, int cardYear, int cardMonth, int CardCvv){
+        Payment(string cardNumber, string cardName, string cardType, int cardYear, int cardMonth, int cardCvv){
             this->cardNumber = cardNumber;
             this->cardName = cardName;
             this->cardType = cardType;
@@ -27,7 +27,31 @@ class Payment{
             this->cardCvv = cardCvv;
         }
 
-        
+        int makePayment(float amount){
+            cout << "Your card is charged for amount Rs. " << amount << endl;
+            cout << "Card Number: " << cardNumber << endl;
+            cout << "Card Holder Name: " << cardName << endl;
+            cout << "Card Type: " << cardType << endl;
+
+
+            int month = getMonth();
+            int year = getYear();
+
+            if(cardYear < year || (cardYear == year && cardMonth < month)){
+                return -1;
+            }
+
+            int cvv;
+            cout << "Enter CVV: ";
+            cin >> cvv;
+
+            if(cvv != cardCvv){
+                return -2;
+            }
+
+            return 0;
+        }
+
 };
 
 class User{
